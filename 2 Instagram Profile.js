36c2f5551ba88075129e11db5aa5810482031c0a
Shubham <152170670+shubhamkfa@users.noreply.github.com>
2024-02-19 22:36:54 +0530

    Add files via upload

---
+++ 2 Instagram Profile.js
@@ -0,0 +1,12 @@
+const profile={
+    name:"Shubham Shinde" ,
+    isFollow: true,
+    button: "Message",
+    isPosts: 195,
+    followers:569,
+    following:4,
+    //Text: "shubham shinde is a best best developr in the India"
+    /* Hello this is comment in Javascript */
+
+
+}
